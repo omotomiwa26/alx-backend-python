60efe6628aa6799315ef2f840d030edff7f8eff6
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-11 06:08:08 +0100

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    This Module Adds a type annotation to the function add
+    that takes a float a and a float b as arguments
+    and returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Return the sum of two floats
+    """
+    return a + b
