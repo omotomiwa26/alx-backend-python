3481a8013fc741134164669ba8beaf2d3657a9a2
omotomiwa26 <omotomiwa26@gmail.com>
2024-10-14 23:06:40 +0100

    0. The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,8 +11,7 @@ import random as r
 async def wait_random(max_delay: int = 10) -> float:
     """
     Delay between 0 and max_delay
-
-    max_delay,with a default value of 10
+    Max_delay,with a default value of 10
     """
     new_rand = r.uniform(0, max_delay)
     await a.sleep(new_rand)
