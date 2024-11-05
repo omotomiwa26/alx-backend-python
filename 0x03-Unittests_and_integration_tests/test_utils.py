0d191fc5d618c8b117e606551f7c652e9ef34867
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-05 01:20:21 +0100

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,4 +43,49 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(Exception):
             access_nested_map(nested_map, path)
-            
+
+
+class TestGetJson(unittest.TestCase):
+    """
+        Mock HTTP calls
+        test to get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, moke_requests_get):
+        """
+            Test_get_json method expected output
+            Args:
+                url: url to send http request
+                plaload: expected json response
+        """
+        moke_requests_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        moke_requests_get.assert_called_once_with(test_url)        
+
+
+class TestMemoize(unittest.TestCase):
+    """
+        Test the memoization decorator, memoize
+    """
+    def test_memoize(self):
+        """
+            Test that the utils.memoize decorator works as intended
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
